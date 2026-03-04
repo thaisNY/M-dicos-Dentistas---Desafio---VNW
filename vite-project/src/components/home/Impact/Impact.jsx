e65f429eabbf9b8bebc413b0262751f69232feab
Thais Rodrigues <thais.th90@gmail.com>
2026-03-04 13:43:04 -0300

    Section Impact

--- vite-project/src/components/home/Impact/Impact.jsx
+++ vite-project/src/components/home/Impact/Impact.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import s from './Impact.module.scss'
+import Card from '../../ui/card/Card'
+
 
 const Impact = () => {
   return (
-    <div>
-      
-    </div>
+    <section className={s.impact}>
+      <h1 className={s.titleImpact}>Nosso Impacto</h1>
+      <p className={s.subtitle}>Transformando a saúde de nossa comunidade, um paciente de cada vez</p>
+
+      <div className={s.cardsContainer}>
+        <Card content={{ title: "2,500+", description: "Pessoas Atendidas" }} variant="card-impact" />
+        <Card content={{ title: "150+", description: "Profissionais Voluntários" }} variant="card-impact" />
+        <Card content={{ title: "98%", description: "Satisfação dos Pacientes" }} variant="card-impact" />
+        <Card content={{ title: "5+", description: "Anos de Dedicação" }} variant="card-impact" />
+      </div>
+
+    </section>
   )
 }
 
